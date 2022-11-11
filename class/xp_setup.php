66ade6be9b5a179e03a41798d88991cbe71e53ef
applh.com <contact@applh.com>
2022-11-12 00:42:58 +0100

    v0.0.2 add admin panel

--- class/xp_setup.php
+++ class/xp_setup.php
@@ -4,6 +4,27 @@ class xp_setup
 {
     static function plugins_loaded ()
     {
+        // https://developer.wordpress.org/reference/functions/add_menu_page/
+        add_action("admin_init", "xp_setup::admin_init");
 
+    }
+
+    static function admin_init ()
+    {
+        // https://developer.wordpress.org/reference/functions/add_menu_page/
+        add_menu_page(
+            "Xpress",
+            "Xpress",
+            "manage_options",
+            "xpress",
+            "xp_setup::xpress_page",
+            "dashicons-admin-generic",
+            100
+        );
+    }
+
+    static function xpress_page ()
+    {
+        require __DIR__ . "/../templates/plugin-admin.php";
     }
 }
