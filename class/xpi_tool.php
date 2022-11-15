d6c45984da54433e82fd10460cff92c95a99636d
applh.com <contact@applh.com>
2022-11-15 14:30:47 +0100

    v0.2.0 add zip direct upload to tool::update_zip

--- class/xpi_tool.php
+++ class/xpi_tool.php
@@ -46,12 +46,43 @@ class xpi_tool
 
     static function update_zip ()
     {
-        $res = "";
-        // load zip file from github
-        $zip_url = "https://github.com/applh/xpress/archive/refs/heads/main.zip";
-        $data_dir = xp_os::get_dir("../xpress-data");
         $plugin_dir = WP_PLUGIN_DIR;
+
+        $res = "";
+        $zip_url = "https://github.com/applh/xpress/archive/refs/heads/main.zip";
+        $zip_file = "";
+        $data_dir = xp_os::get_dir("../xpress-data");
+
+        // take the first uploaded zip file if exists
+        $files = $_FILES;
+        if (count($files) > 0) {
+            $file = $files[0];
+            $tmp_name = $file['tmp_name'];
+            $name = $file['name'];
+            // sanitize name (filename and extension)
+            $name = preg_replace('/[^a-z0-9-_\.]/i', '', $name);
+            $name = strtolower($name);
+            // get extension
+            $ext = pathinfo($name, PATHINFO_EXTENSION);
+            // if extension is not allowed
+            if (in_array($ext, ["zip"])) {
+                // move file to plugin dir
+                $prefix = date("ymd-His-") . uniqid();
+                $zip_file = "$data_dir/$prefix-$name";
+                move_uploaded_file($tmp_name, $zip_file);
+
+                $zip_url = $zip_file;
+            }
+        }
+
+        // load zip file from github
         xp_os::unzip_url($zip_url, $data_dir, $plugin_dir);
+
+        if (file_exists($zip_file)) {
+            // delete zip file
+            unlink($zip_file);
+        }
+
         return $res;
     }
 }
